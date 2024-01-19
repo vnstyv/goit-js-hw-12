1806839cd53fbdbba8bbbc8e297f00a8ab0cf5a7
Anastasia Romanyuk <vnstyv@gmail.com>
2024-01-20 00:18:26 +0200

    edits

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ const pageForm = document.querySelector(".form");
 const pageLoaderTop = document.querySelector(".loader-top");
 const pageLoaderBottom = document.querySelector(".loader-bottom");
 const pageGallery = document.querySelector(".gallery");
-const pageLoadImg = document.querySelector('.load-image');
+const pageLoadImg = document.querySelector(".load-image");
 
 
 let page = 1;
@@ -24,23 +24,23 @@ const lightbox = new SimpleLightbox(".gallery a", {
     docClose: true
     });
 
-pageForm.addEventListener('submit', onSubmit);
-pageLoadImg.addEventListener('click', loadMore);
+pageForm.addEventListener("submit", onSubmit);
+pageLoadImg.addEventListener("click", loadMore);
 
 async function onSubmit(event) {
     event.preventDefault();
-    pageLoaderTop.style.display = 'block';
-    pageLoadImg.style.display = 'none';
+    pageLoaderTop.style.display = "block";
+    pageLoadImg.style.display = "none";
     page = 1;
     q = event.target.elements.search.value.trim();
 
       if (!q) {
     pageGallery.innerHTML = '';
     iziToast.info({
-      position: 'topRight',
-      message: 'Error enter any symbols',
+      position: "topRight",
+      message: `Error enter any symbols`,
     });
-    pageLoaderTop.style.display = 'none';
+    pageLoaderTop.style.display = "none";
     return;
     }
     
@@ -50,43 +50,43 @@ async function onSubmit(event) {
     } = await searchImg(q, page);
         
     if (hits.length > 0) {
-    pageLoaderTop.style.display = 'none';
+    pageLoaderTop.style.display = "none";
     pageGallery.innerHTML = renderImg(hits);
     lightbox.refresh();
     iziToast.success({
-    position: 'topRight',
+    position: "topRight",
     message: `We found ${totalHits} photos`,
     });
-    pageLoadImg.style.display = 'block';
+    pageLoadImg.style.display = "block";
     } else {
     pageGallery.innerHTML = '';
       iziToast.error({
-        position: 'topRight',
+        position: "topRight",
         message:
-          'Sorry, there are no images matching your search query. Please try again!',
+          `Sorry, there are no images matching your search query. Please try again!`,
       });
         }
         
     if (totalHits <= per_page) {
-      pageLoadImg.style.display = 'none';
+      pageLoadImg.style.display = "none";
     }
     } catch (error) {
-        console.log('Error');
+        console.log("Error");
     } finally {
-        pageLoaderTop.style.display = 'none';
+        pageLoaderTop.style.display = "none";
         event.target.reset();
     }
 }
 
 function searchImg(q, page) {
-  axios.defaults.baseURL = 'https://pixabay.com';
+  axios.defaults.baseURL = "https://pixabay.com";
 
-  return axios.get('/api/', {
+  return axios.get("/api/", {
     params: {
-      key: '34228101-50b55348103eeb6dd10b59f8d',
+      key: "34228101-50b55348103eeb6dd10b59f8d",
       q,
-      image_type: 'photo',
-      orientation: 'horizontal',
+      image_type: "photo",
+      orientation: "horizontal",
       safesearch: true,
       page,
       per_page: 40,
@@ -114,9 +114,9 @@ function renderImg(hits = []) {
 }
 
 async function loadMore(event) {
-  pageLoaderBottom.style.display = 'block';
-  loadImg.style.display = 'none';
-  const listItem = document.querySelector('.gallery-item:first-child');
+  pageLoaderBottom.style.display = "block";
+  pageLoadImg.style.display = "none";
+  const listItem = document.querySelector(".gallery-item:first-child");
   const itemHeight = listItem.getBoundingClientRect().height;
 
   try {
@@ -127,24 +127,24 @@ async function loadMore(event) {
     } = await searchImg(q, page);
     const totalPage = Math.ceil(totalHits / per_page);
 
-    pageLoadImg.style.display = 'block';
-    pageGallery.insertAdjacentHTML('beforeend', renderImg(hits));
+    pageLoadImg.style.display = "block";
+    pageGallery.insertAdjacentHTML("beforeend", renderImg(hits));
     lightbox.refresh();
 
     if (page === totalPage) {
-      pageLoadImg.style.display = 'none';
+      pageLoadImg.style.display = "none";
       return iziToast.info({
-        position: 'topRight',
+        position: "topRight",
         message: `We're sorry, but you've reached the end of search results.`,
       });
     }
   } catch (error) {
     console.log(error);
   } finally {
-    pageLoaderBottom.style.display = 'none';
+    pageLoaderBottom.style.display = "none";
     window.scrollBy({
       top: 2 * itemHeight,
-      behavior: 'smooth',
+      behavior: "smooth",
     });
   }
 }
